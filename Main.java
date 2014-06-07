627e825e956a1008e7ee0df76f684268983631ed
AnythingModders <bwfcwalshy@gmail.com>
2014-06-07 15:01:53 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,2 +1,22 @@
-VoteCommand-And3210123-
-=======================
+package com.bwfcwalshy.main;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+import com.bwfcwalshy.main.commands.Vote;
+
+public class Main extends JavaPlugin {
+
+	@Override
+	public void onEnable() {
+		getLogger().info("Plugin enabled");
+		super.onEnable();
+		
+		getCommand("vote").setExecutor(new Vote());
+	}
+	
+	@Override
+	public void onDisable() {
+		getLogger().info("Plugin Disabled");
+		super.onDisable();
+	}
+}
